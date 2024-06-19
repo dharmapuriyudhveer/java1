db8a3f0bf904ab2baf68cd9b98fde4b9415b4745
dharmapuriyudhveer <dharmapuriyudhveer@gmail.com>
2024-06-19 23:03:09 +0530

    Yep

---
+++ if7calci.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class if7calci 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter the value of a");
+        int a=sc.nextInt();
+
+        System.out.println("Enter the value of b");
+        int b=sc.nextInt();
+
+        System.out.println("Enter the operator :");
+        char c=sc.next().charAt(0);
+
+        if(c == '+')
+            System.out.println(a + b);
+        else if(c == '-')
+            System.out.println(a-b);
+        else if(c == '*')
+            System.out.println(a*b);
+        else if(c == '/')
+            System.out.println(a/b);
+        else
+            System.out.println("Invalid operator");
+    }
+}

3f63080c7ad0b85a9ef6513da2878344773b9e0f
dharmapuriyudhveer <dharmapuriyudhveer@gmail.com>
2024-06-19 22:14:16 +0530

    Yup

---
+++ if5odd.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class if5odd 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter the value :");
+        int a=sc.nextInt();
+
+        if(a%2==0)
+            System.out.println(a + " is an even number");
+        else
+            System.out.println(a + " is an odd number");
+    }
+}

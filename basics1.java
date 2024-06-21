0ef40f639a72c437efcccb6588fd0e4b646781e7
dharmapuriyudhveer <dharmapuriyudhveer@gmail.com>
2024-06-21 17:50:15 +0530

    Yup

---
+++ basics1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class basics1 
+{
+    public static void main(String[] args) 
+    {
+        //Approach 1:
+        // System.out.println("ENter your favourite number :");
+
+        // Scanner sc=new Scanner(System.in);
+        // int i=sc.nextInt();
+
+        // System.out.println(i + " is my favourite number too !!");
+
+        
+        //Approach 2:
+        // Scanner sc=new Scanner(System.in);
+
+        // System.out.println("ENter your favourite number :");
+        // int i=sc.nextInt();
+
+        // System.out.println(i + " is my favourite number too !!");
+
+        //Approach 3:
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("ENter your favourite number :");
+
+        System.out.println(sc.nextInt() + " is my favourite number too !!");
+        
+    }
+}
